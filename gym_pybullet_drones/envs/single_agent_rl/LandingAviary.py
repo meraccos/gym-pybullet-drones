a1802749283318a1230398f86de93de73e93f10d
Meraj Mammadov <meraccos@gmail.com>
2022-04-28 10:45:27 +0000

    working

--- gym_pybullet_drones/envs/single_agent_rl/LandingAviary.py
+++ gym_pybullet_drones/envs/single_agent_rl/LandingAviary.py
@@ -17,7 +17,7 @@ class LandingAviary(BaseSingleAgentAviary):
                  physics: Physics=Physics.PYB,
                  freq: int= 240,
                  aggregate_phy_steps: int=10,
-                 gui=False,
+                 gui=True,
                  record=True, 
                  obs: ObservationType=ObservationType.RGB,
                  act: ActionType=ActionType.VEL,
